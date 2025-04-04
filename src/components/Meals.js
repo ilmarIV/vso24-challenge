c7cdd44cde103b87411d948e6b7aeca145b046ef
VI <ilmar.vodi@voco.ee>
2025-04-04 10:23:46 +0300

    able to display meal data in console

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 const Meals = () => {
+
+    useEffect(() => {
+        fetch("http://localhost:3001/meals")
+        .then(res => res.json())
+        .then(data => {
+          console.log("Meals data:", data);
+        })
+    });
+
     return (
         <ul id="meals">
             { 
