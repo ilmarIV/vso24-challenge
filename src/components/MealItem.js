bc6383655f5de8ff027db1d1a0acc02371408efb
VI <ilmar.vodi@voco.ee>
2025-04-04 11:31:19 +0300

    task5 currenct symbol done

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,11 +1,14 @@
 const MealItem = (props) => {
+
+    const formattedPrice = new Intl.NumberFormat("et-EE", { style: "currency", currency: "EUR" }).format(props.meal.price,)
+
     return (
         <li className="meal-item">
             <article>
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3>{props.meal.name}</h3>
-                    <p className="meal-item-price">{props.meal.price}</p>
+                    <p className="meal-item-price">{formattedPrice}</p>
                     <p className="meal-item-description">{props.meal.description}</p>
                 </div>
                 <p>
