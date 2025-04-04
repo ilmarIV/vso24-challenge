2ff8789399e3f154bb13872eaf85d088e79915ce
VI <ilmar.vodi@voco.ee>
2025-04-04 11:20:26 +0300

    task4 done

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,19 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import MealItem from "./MealItem";
 
 const Meals = () => {
+
+    let [meals, setMeals] = useState([]);
 
     useEffect(() => {
         fetch("http://localhost:3001/meals")
         .then(res => res.json())
         .then(data => {
-          console.log("Meals data:", data);
-        })
-    });
+            console.log("Meals data:", data);
+            setMeals(data);
+        }).catch((err) => {
+            console.error("Error fetching meals:", err);
+        });
+    }, []);
 
     return (
         <ul id="meals">
-            { 
-                // list of meals
+            {
+                meals.map((meal) => {
+                    return <MealItem key={meal.id} meal={meal} />
+                })
             }
         </ul>
     )
